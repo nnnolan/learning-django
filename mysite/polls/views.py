42394b1390cc188dbb5d2625ca5aa920ee14af5d
Nolan <pestanonolan@gmail.com>
2022-02-22 09:05:45 -0600

    wtf is django

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
 from .models import Question
 
+
 # Create your views here.
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/details.html", {"question": question})
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -18,3 +30,7 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {"question" : question})   
+    
