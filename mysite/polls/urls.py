c998084f60df73fc68633b89efc20976d4d61b1e
Nolan <pestanonolan@gmail.com>
2022-02-24 09:05:52 -0600

    tests cases
    uwuuuu

---
+++ mysite/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.http import HttpResponse
+
+from . import views, urls
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
