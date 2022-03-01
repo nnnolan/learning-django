08604fdcbf5be0e8321f50d192a49511b7db4870
Nolan <pestanonolan@gmail.com>
2022-03-01 08:06:05 -0600

    push

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -71,7 +71,7 @@ class QuestionIndexViewTests(TestCase):
         )
     
     def test_two_past_questions(self):
-        #the questions index page may display multipel questions.
+        #the questions index page may display multiple questions.
         question1 = create_question(question_text="Past question 1.", days=-30)
         question2 = create_question(question_text="Past question 2.", days=-5)
         response = self.client.get(reverse('polls:index'))
