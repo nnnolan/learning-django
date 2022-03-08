bc524669f55b412d2971141877d313b80c8212b4
Nolan <pestanonolan@gmail.com>
2022-03-08 14:14:14 -0600

    psuh

--- sabre/elo/views.py
+++ sabre/elo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
 
